a3fbbbc4788331d0069b14246a791a19d9dbb217
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2019-04-02 21:46:24 +0200

    [DRAFT] Erste Prozesse und Task-Worker für .NET

---
+++ ExternalTasks/EtikettAusdrucken/DotNet/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EtikettAusdrucken
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Foundation.IAM.Contracts;
+    using Newtonsoft.Json;
+    using ProcessEngine.ExternalTaskAPI.Client;
+    using ProcessEngine.ExternalTaskAPI.Contracts;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ExternalTasks/EtikettAusdrucken/DotNet/TestIdenitity.cs
@@ -0,0 +1,15 @@
+namespace EtikettAusdrucken
+{
+    using System;
+
+    using Foundation.IAM.Contracts;
+    
+
+    internal class TestIdentity: IIdentity
+    {
+        public override string ToString()
+        {
+            return "ZHVtbXlfdG9rZW4=";
+        }
+    }
+}
