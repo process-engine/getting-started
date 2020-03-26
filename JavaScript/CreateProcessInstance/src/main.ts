6181b7330078010781f96f373d92f38fd53e4f5a
ElRaptorus <christian.werner@5minds.de>
2020-03-26 08:54:22 +0100

    :lipstick: Style improvements

--- JavaScript/CreateProcessInstance/src/main.ts
+++ JavaScript/CreateProcessInstance/src/main.ts
@@ -14,29 +14,10 @@ const processModelId = 'Benutzeraktivierung';
 const startEventId = 'StartAktivierung';
 const endEventId = 'EndeAktivierung';
 
-function createConsumerClient(url: string): ConsumerApiClient {
-  const httpClient = new HttpClient();
-  httpClient.config = {url: url};
-
-  const externalAccessor = new ExternalAccessor(httpClient);
-  const client = new ConsumerApiClient(externalAccessor);
-
-  return client;
-}
-
-function createPayload(shoppingCardAmount: number): DataModels.ProcessModels.ProcessStartRequestPayload {
-  const processStartPayload = new DataModels.ProcessModels.ProcessStartRequestPayload();
-  processStartPayload.inputValues = {
-    shoppingCardAmount: shoppingCardAmount,
-  };
-
-  return processStartPayload;
-}
-
 async function main(): Promise<void> {
-  const client = createConsumerClient('http://localhost:8000');
+  const client = createConsumerApiClient('http://localhost:8000');
 
-  const processStartPayload = createPayload(1000);
+  const processStartPayload = createProcessStartPayload(1000);
 
   console.log(`Prozess '${processModelId}' mit Start Event '${startEventId}' gestartet`);
 
@@ -46,12 +27,32 @@ async function main(): Promise<void> {
     processStartPayload,
     DataModels.ProcessModels.StartCallbackType.CallbackOnEndEventReached,
     startEventId,
-    endEventId);
+    endEventId,
+  );
 
   console.log('Prozess beendet');
   console.log('Correlation ID: ', result.correlationId)
   console.log('Ergebnis: ');
   console.log(result.tokenPayload);
+}
+
+function createConsumerApiClient(url: string): ConsumerApiClient {
+  const httpClient = new HttpClient();
+  httpClient.config = {url: url};
+
+  const externalAccessor = new ExternalAccessor(httpClient);
+  const client = new ConsumerApiClient(externalAccessor);
+
+  return client;
+}
+
+function createProcessStartPayload(shoppingCardAmount: number): DataModels.ProcessModels.ProcessStartRequestPayload {
+  const processStartPayload = new DataModels.ProcessModels.ProcessStartRequestPayload();
+  processStartPayload.inputValues = {
+    shoppingCardAmount: shoppingCardAmount,
+  };
+
+  return processStartPayload;
 }
 
 main();
